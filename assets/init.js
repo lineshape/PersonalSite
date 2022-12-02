a2999a98bc8156321823efab49ad85ee21799be9
lineshape <94829107+lineshape@users.noreply.github.com>
2022-12-02 17:00:40 +0900

    이것저것

--- assets/init.js
+++ assets/init.js
@@ -6,9 +6,8 @@ ready for the code inside of the
 function to be executed 
 */
 
-$(document).ready(function() {
-    
-    /* 
+$(document).ready(function () {
+  /* 
     jquery follows its own syntax. it uses
     the $ symbol to look for an element. 
     followed by () which is the element to find
@@ -18,105 +17,105 @@ $(document).ready(function() {
     inside the curly brackets.
     */
 
-    // background-color change
+  // background-color change
 
-    $('p .work-workers').mouseover(function(){
-        $('body').css('background', '#3CFFFF');
-        $('.grad-left-black').css('opacity', '0');
-        $('.grad-left-white').css('opacity', '100');
-        $('.grad-right-black').css('opacity', '0');
-        $('.grad-right-white').css('opacity', '100'); 
-        $('.workers').css('bottom','100px');
-         // 시안, 일꾼들
-    })
+  $('p .work-workers').mouseover(function () {
+    $('body').css('background', '#3CFFFF')
+    $('.grad-left-black').css('opacity', '0')
+    $('.grad-left-white').css('opacity', '100')
+    $('.grad-right-black').css('opacity', '0')
+    $('.grad-right-white').css('opacity', '100')
+    $('.workers').css('bottom', '100px')
+    // 시안, 일꾼들
+  })
 
-    $('p .work-workers').mouseout(function(){
-        $('body').css('background', '#FFFFFF');
-        $('.grad-left-black').css('opacity', '100');
-        $('.grad-left-white').css('opacity', '0');
-        $('.grad-right-black').css('opacity', '100');
-        $('.grad-right-white').css('opacity', '0');
-        $('.workers').css('bottom','-999px');
-    })
+  $('p .work-workers').mouseout(function () {
+    $('body').css('background', '#FFFFFF')
+    $('.grad-left-black').css('opacity', '100')
+    $('.grad-left-white').css('opacity', '0')
+    $('.grad-right-black').css('opacity', '100')
+    $('.grad-right-white').css('opacity', '0')
+    $('.workers').css('bottom', '-999px')
+  })
 
-    $('p .work-microworld').mouseover(function(){
-        $('body').css('background', '#FB25FF');
-        $('.grad-left-black').css('opacity', '0');
-        $('.grad-left-white').css('opacity', '100');
-        $('.grad-right-black').css('opacity', '0');
-        $('.grad-right-white').css('opacity', '100'); // 마젠타, 미시세계체
-    })
+  $('p .work-microworld').mouseover(function () {
+    $('body').css('background', '#FB25FF')
+    $('.grad-left-black').css('opacity', '0')
+    $('.grad-left-white').css('opacity', '100')
+    $('.grad-right-black').css('opacity', '0')
+    $('.grad-right-white').css('opacity', '100')
+    $('.microworld').css('top', '50px') // 마젠타, 미시세계체
+  })
 
-    $('p .work-microworld').mouseout(function(){
-        $('body').css('background', '#FFFFFF');
-        $('.grad-left-black').css('opacity', '100');
-        $('.grad-left-white').css('opacity', '0');
-        $('.grad-right-black').css('opacity', '100');
-        $('.grad-right-white').css('opacity', '0');
-    })
+  $('p .work-microworld').mouseout(function () {
+    $('body').css('background', '#FFFFFF')
+    $('.grad-left-black').css('opacity', '100')
+    $('.grad-left-white').css('opacity', '0')
+    $('.grad-right-black').css('opacity', '100')
+    $('.grad-right-white').css('opacity', '0')
+    $('.microworld').css('top', '-999px')
+  })
 
-    $('p .work-sheti').mouseover(function(){
-        $('body').css('background', '#FF5F5F');
-        $('.grad-left-black').css('opacity', '0');
-        $('.grad-left-white').css('opacity', '100');
-        $('.grad-right-black').css('opacity', '0');
-        $('.grad-right-white').css('opacity', '100');
-        $('.sheti').css('left','700px'); // 다홍, sheti
-    })
-    $('p .work-sheti').mouseout(function(){
-        $('body').css('background', '#FFFFFF');
-        $('.grad-left-black').css('opacity', '100');
-        $('.grad-left-white').css('opacity', '0');
-        $('.grad-right-black').css('opacity', '100');
-        $('.grad-right-white').css('opacity', '0');
-        $('.sheti').css('left','-999px');
-    })
+  $('p .work-sheti').mouseover(function () {
+    $('body').css('background', '#FF5F5F')
+    $('.grad-left-black').css('opacity', '0')
+    $('.grad-left-white').css('opacity', '100')
+    $('.grad-right-black').css('opacity', '0')
+    $('.grad-right-white').css('opacity', '100')
+    $('.sheti').css('left', '700px') // 다홍, sheti
+  })
+  $('p .work-sheti').mouseout(function () {
+    $('body').css('background', '#FFFFFF')
+    $('.grad-left-black').css('opacity', '100')
+    $('.grad-left-white').css('opacity', '0')
+    $('.grad-right-black').css('opacity', '100')
+    $('.grad-right-white').css('opacity', '0')
+    $('.sheti').css('left', '-999px')
+  })
 
-    $('p .work-elephant').mouseover(function(){
-        $('body').css('background', '#3CFF8A');
-        $('.grad-left-black').css('opacity', '0');
-        $('.grad-left-white').css('opacity', '100');
-        $('.grad-right-black').css('opacity', '0');
-        $('.grad-right-white').css('opacity', '100');
-        $('.elephant').css('top','-50px'); // 그린, 코끼리
-    })
-    $('p .work-elephant').mouseout(function(){
-        $('body').css('background', '#FFFFFF');
-        $('.grad-left-black').css('opacity', '100');
-        $('.grad-left-white').css('opacity', '0');
-        $('.grad-right-black').css('opacity', '100');
-        $('.grad-right-white').css('opacity', '0');
-        $('.elephant').css('top','-999px');
-    })
+  $('p .work-elephant').mouseover(function () {
+    $('body').css('background', '#3CFF8A')
+    $('.grad-left-black').css('opacity', '0')
+    $('.grad-left-white').css('opacity', '100')
+    $('.grad-right-black').css('opacity', '0')
+    $('.grad-right-white').css('opacity', '100')
+    $('.elephant').css('top', '-50px') // 그린, 코끼리
+  })
+  $('p .work-elephant').mouseout(function () {
+    $('body').css('background', '#FFFFFF')
+    $('.grad-left-black').css('opacity', '100')
+    $('.grad-left-white').css('opacity', '0')
+    $('.grad-right-black').css('opacity', '100')
+    $('.grad-right-white').css('opacity', '0')
+    $('.elephant').css('top', '-999px')
+  })
 
-    $('p .work-weekly').mouseover(function(){
-        $('body').css('background', '#FBFF3C');
-        $('.grad-left-black').css('opacity', '0');
-        $('.grad-left-white').css('opacity', '100');
-        $('.grad-right-black').css('opacity', '0');
-        $('.grad-right-white').css('opacity', '100');
-        $('.weekly').css('left','200px'); // 노랑, 주간생각
-    })
-    $('p .work-weekly').mouseout(function(){
-        $('body').css('background', '#FFFFFF');
-        $('.grad-left-black').css('opacity', '100');
-        $('.grad-left-white').css('opacity', '0');
-        $('.grad-right-black').css('opacity', '100');
-        $('.grad-right-white').css('opacity', '0');
-        $('.weekly').css('left','-999px');
-    })
+  $('p .work-weekly').mouseover(function () {
+    $('body').css('background', '#FBFF3C')
+    $('.grad-left-black').css('opacity', '0')
+    $('.grad-left-white').css('opacity', '100')
+    $('.grad-right-black').css('opacity', '0')
+    $('.grad-right-white').css('opacity', '100')
+    $('.weekly').css('left', '200px') // 노랑, 주간생각
+  })
+  $('p .work-weekly').mouseout(function () {
+    $('body').css('background', '#FFFFFF')
+    $('.grad-left-black').css('opacity', '100')
+    $('.grad-left-white').css('opacity', '0')
+    $('.grad-right-black').css('opacity', '100')
+    $('.grad-right-white').css('opacity', '0')
+    $('.weekly').css('left', '-999px')
+  })
 
-    // language toggle
+  // language toggle
 
-    $('.kor').click(function(){
-        $('.eng-intro').css('display', 'none');
-        $('.kor-intro').css('display', 'block');
-    })
+  $('.kor').click(function () {
+    $('.eng-intro').css('display', 'none')
+    $('.kor-intro').css('display', 'block')
+  })
 
-    $('.eng').click(function(){
-        $('.kor-intro').css('display', 'none');
-        $('.eng-intro').css('display', 'block');
-    })
-
-
-});
+  $('.eng').click(function () {
+    $('.kor-intro').css('display', 'none')
+    $('.eng-intro').css('display', 'block')
+  })
+})
